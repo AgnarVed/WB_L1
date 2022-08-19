fa500f4f4e8f7fa1f502cad8d1bb85bd59f01822
Valentin Yun <v.iun@magnus-tech.ru>
2022-08-19 10:41:25 +0300

    third

--- PracticeQuestions/Q4/Q4.go
+++ PracticeQuestions/Q4/Q4.go
@@ -19,89 +19,67 @@ import (
 //Программа должна завершаться по нажатию Ctrl+C. Выбрать и обосновать способ завершения работы всех воркеров.
 
 func main() {
-	n := 5
-	var err error
-	ctx, cancel := context.WithCancel(context.Background())
+	var n int     // кол-во воркеров
+	var err error // ошибка
+
+	ctx, cancel := signal.NotifyContext(context.Background(), os.Signal(syscall.SIGINT))
 	defer cancel()
 
+	// проверка аргументов
 	if len(os.Args) == 2 {
 		n, err = strconv.Atoi(os.Args[1])
-		if err != nil || n == 0 {
-			fmt.Println("You need to write a number starting from 1")
+		if err != nil || n < 0 {
+			fmt.Println("Введите число от 1")
 			return
 		}
 	} else {
-		fmt.Println("You need to specify 1 argument - amount of workers")
+		fmt.Println("Уточните кол-во воркеров")
 		return
 	}
 
-	workers := NewWorkers(n, cancel)
-	workers.work(ctx)
+	workersCh := make(chan interface{}, n)
 
-	signals := make(chan os.Signal, 1)
-	signal.Notify(signals, syscall.SIGINT, syscall.SIGTERM, syscall.SIGQUIT)
+	var wg sync.WaitGroup
+	work(ctx, workersCh, n, &wg)
 
-	generator := rand.New(rand.NewSource(time.Now().UnixNano()))
-	for {
-		num := generator.Intn(1000)
-		chars := 'a' + rune(generator.Intn('z'-'a'+1))
-		select {
-		case s := <-signals: // при получении сигнала завершаем работу всех воркеров
-			fmt.Printf("\nProgramm has been stopped by signal %d\n", s)
-			workers.stopWork()
-
-			return
-		default: // посылаем данные постоянно
-			workers.sendData(num)
-			workers.sendData(string(chars))
+	go func() {
+		generator := rand.New(rand.NewSource(time.Now().UnixNano()))
+		for {
+			num := generator.Intn(100)
+			//chars := 'a' + rune(generator.Intn('z'-'a'+1))
+			select {
+			case <-ctx.Done(): // при получении сигнала завершаем работу всех воркеров
+				fmt.Printf("Programm has been stopped by signal\n")
+				close(workersCh)
+				return
+			default: // посылаем данные постоянно
+				workersCh <- num
+				//workersCh <- string(chars)
+			}
+			time.Sleep(time.Millisecond * 300)
 		}
-		time.Sleep(time.Millisecond * 100)
-	}
+	}()
 
+	wg.Wait() // wg
+	// закрываем канал
+	fmt.Println("\nAll workers have been stopped")
 }
 
-type Workers struct {
-	ch     chan interface{}   // канал
-	wg     sync.WaitGroup     // wg
-	cancel context.CancelFunc // отмена
-	n      int                // кол-во
-}
-
-func NewWorkers(n int, ctx context.CancelFunc) *Workers {
-	return &Workers{
-		ch:     make(chan interface{}, n),
-		cancel: ctx,
-		n:      n,
-	}
-}
-
-func (w *Workers) work(ctx context.Context) {
-	for i := 0; i < w.n; i++ {
-		w.wg.Add(1)
+func work(ctx context.Context, ch <-chan interface{}, n int, wg *sync.WaitGroup) {
+	for i := 0; i < n; i++ {
+		wg.Add(1)
 		go func(id int) {
+			defer wg.Done()
 			for {
 				select {
 				// из канала пишем в data
-				case data := <-w.ch:
-					fmt.Printf("Worker %d read data from channel: Type %T, Value %v\n", id+1, data, data)
-				// при получении Done пишем о завершении
+				case data := <-ch:
+					fmt.Printf("Worker %d обработал: Type %T, Value %v\n", id+1, data, data)
+					// при получении Done
 				case <-ctx.Done():
-					fmt.Println("Worker ", id+1, " stopped")
 					return
 				}
 			}
-			w.wg.Done()
 		}(i)
 	}
-}
-
-func (w *Workers) stopWork() {
-	//w.cancel()  // ctx отмены
-	w.wg.Wait() // wg
-	close(w.ch) // закрываем канал
-	fmt.Println("All workers have been stopped")
-}
-
-func (w *Workers) sendData(data interface{}) {
-	w.ch <- data // отправляем данные в канал
 }
