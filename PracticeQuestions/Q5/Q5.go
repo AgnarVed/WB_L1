b3be0c291aef92bf691e6e48bd5a80dae6395298
Valentin Yun <v.iun@magnus-tech.ru>
2022-08-19 10:55:04 +0300

    Q5 finished

--- PracticeQuestions/Q5/Q5.go
+++ PracticeQuestions/Q5/Q5.go
@@ -1,5 +1,34 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"math/rand"
+	"time"
+)
 
+// Разработать программу, которая будет последовательно отправлять значения в канал,
+//а с другой стороны канала — читать. По истечению N секунд программа должна завершаться.
+
+func main() {
+	var duration int
+	duration = 5
+	msgChan := make(chan interface{})
+	defer close(msgChan)
+
+	finishTime := time.Duration(duration) * time.Second
+
+	startTime := time.Now()
+
+	go func(ch chan interface{}) {
+		for val := range ch {
+			fmt.Println("Value is: ", val)
+		}
+	}(msgChan)
+
+	generator := rand.New(rand.NewSource(time.Now().UnixNano()))
+	for time.Since(startTime) < finishTime {
+		val := generator.Intn(100)
+		msgChan <- val
+		time.Sleep(500 * time.Millisecond)
+	}
 }
