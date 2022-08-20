97c1aacc2ee8ffe0d4294ea3134efbea831750b9
Valentin Yun <v.iun@magnus-tech.ru>
2022-08-20 20:03:45 +0300

    Q5

--- PracticeQuestions/Q5/Q5.go
+++ PracticeQuestions/Q5/Q5.go
@@ -10,25 +10,27 @@ import (
 //а с другой стороны канала — читать. По истечению N секунд программа должна завершаться.
 
 func main() {
-	var duration int
+	var duration int // время выполнения
 	duration = 5
-	msgChan := make(chan interface{})
-	defer close(msgChan)
+	msgChan := make(chan interface{}) // канал
+	defer close(msgChan)              // закрытие канала
 
-	finishTime := time.Duration(duration) * time.Second
+	finishTime := time.Duration(duration) * time.Second // время окончания
 
-	startTime := time.Now()
+	startTime := time.Now() // время начала
 
+	// горутина, что выводит данные из канала
 	go func(ch chan interface{}) {
 		for val := range ch {
 			fmt.Println("Value is: ", val)
 		}
 	}(msgChan)
 
+	// генератор случайных значений
 	generator := rand.New(rand.NewSource(time.Now().UnixNano()))
 	for time.Since(startTime) < finishTime {
 		val := generator.Intn(100)
-		msgChan <- val
+		msgChan <- val // отправка значений в канал
 		time.Sleep(500 * time.Millisecond)
 	}
 }
