777fe3eb18c972d4ea6ddbaf557a3121cc152444
Valentin Yun <v.iun@magnus-tech.ru>
2022-08-22 11:49:15 +0300

    Q1 finished

--- PracticeQuestions/Q1/Q1.go
+++ PracticeQuestions/Q1/Q1.go
@@ -5,6 +5,7 @@ import "fmt"
 //Дана структура Human (с произвольным набором полей и методов).
 //Реализовать встраивание методов в структуре Action от родительской структуры Human (аналог наследования).
 
+// Структура Человек
 type Human struct {
 	Name string
 	Age  int
@@ -12,19 +13,23 @@ type Human struct {
 	Wait
 }
 
+// Ожидание
 type Wait struct {
 	Time int
 }
 
+// Действие
 type Action struct {
 	Walk int
 }
 
+// Сделать действие (упражнения)
 func (a *Action) DoSomething() string {
 	return "do exercises"
 }
 
 func main() {
+	// Человек р1
 	p1 := Human{
 		Name: "Bob",
 		Age:  23,
@@ -38,16 +43,20 @@ func main() {
 	fmt.Printf("%d", p1.days(2))
 }
 
+// Например, нужно узнать, сколько времени нужно делать упражнения, чтобы достичь результата
 func (a *Action) days(input int) int {
 	result := input * 60
 	return result
 }
 
+// Но также узнать, сколько нужно времени, чтобы успеть отдохнуть
 func (w *Wait) days(input int) int {
 	result := input * 24
 	return result
 }
 
+// Указываем, что при запросе "days" возвращалось кол-во времени, в течении которого нужно выполнять упражнения,
+// т.к. происходит наложение ( ambiguous reference )
 func (h *Human) days(input int) int {
 	return h.Action.days(input)
 }
